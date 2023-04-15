2c24b496da92b714c75f3056fe3dd4e28b7146ec
Vlad Makhnenko <vlmakh@gmail.com>
2023-04-15 14:42:20 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -11,10 +11,21 @@ export const coinList = async page => {
   return response.data;
 };
 
-export const singleCoin = id => `https://api.coingecko.com/api/v3/coins/${id}`;
+export const singleCoin = async id => {
+  const response = await axios.get(`${MAIN_URL}/${id}`);
+  return response.data;
+};
 
-export const historicalChart = (id, days = 365) =>
-  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+export const historicalChart = async (id, days = 365) => {
+  const response = await axios.get(
+    `${MAIN_URL}/${id}/market_chart?vs_currency=${currency}&days=${days}`
+  );
+  return response.data;
+};
 
-export const trendingCoins = () =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+export const trendingCoins = async page => {
+  const response = await axios.get(
+    `${MAIN_URL}/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=${page}`
+  );
+  return response.data;
+};
