55e2379772bb02a2584f71c39abcaaeb6ee880f4
Vlad Makhnenko <vlmakh@gmail.com>
2023-04-18 15:07:49 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,29 +4,29 @@ const MAIN_URL = process.env.REACT_APP_MAIN_URL;
 
 const currency = 'USD';
 
-export const coinList = async page => {
+const coinList = async page => {
   const response = await axios.get(
-    `${MAIN_URL}/markets?vs_currency=USD&per_page=10&page=${page}`
+    `${MAIN_URL}/coins/markets?vs_currency=USD&per_page=10&page=${page}`
   );
   return response.data;
 };
 
-export const singleCoin = async id => {
+const singleCoin = async id => {
   const response = await axios.get(`${MAIN_URL}/${id}`);
   return response.data;
 };
 
-export const historicalChart = async (id, days = 365) => {
+const historicalChart = async (id, days = 365) => {
   const response = await axios.get(
-    `${MAIN_URL}/${id}/market_chart?vs_currency=${currency}&days=${days}`
+    `${MAIN_URL}/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`
   );
   return response.data;
 };
 
-export const userWatchList = async array => {
+const userWatchList = async array => {
   const arrayOfCoins = array.map(async coinId => {
     return await axios
-      .get(`${MAIN_URL}/${coinId}`)
+      .get(`${MAIN_URL}/coins/${coinId}`)
       .then(response => {
         return response.data;
       })
@@ -36,3 +36,10 @@ export const userWatchList = async array => {
   const response = await Promise.all(arrayOfCoins);
   return response;
 };
+
+const searchCoin = async query => {
+  const response = await axios.get(`${MAIN_URL}/search/?query=${query}`);
+  return response.data;
+};
+
+export { coinList, singleCoin, historicalChart, userWatchList, searchCoin };
